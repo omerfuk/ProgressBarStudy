6404b02b1cdeae26a0b7a7343b1e5522f1d46521
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-11 14:06:55 +0300

    dynamic progress bar study with using timer

--- ProgressBarStudy/ViewController.swift
+++ ProgressBarStudy/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: ProgressBar!
+    
+    var countFired: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            
+            self.countFired += 1
+            
+            DispatchQueue.main.async {
+                self.progressBar.progress = min(0.03 * self.countFired, 1)
+                
+                if self.progressBar.progress == 1 {
+                    timer.invalidate()
+                }
+            }
+        }
     }
 
 
